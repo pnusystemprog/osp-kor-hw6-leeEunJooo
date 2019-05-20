1e04dc6022dd57a6260fe6e9d2abe6a6c8b9417a
leeEunJooo <dmswn334000@naver.com>
2019-05-20 08:15:48 -0700

    edit ++ in regi.c

--- printAll.c
+++ printAll.c
@@ -7,7 +7,7 @@ extern struct Contact PhoneBook[MAX];
 void printAll()
 {
 	printf("<<Display all contacts in the PhoneBook>>\n");
-	//for(int i = 0; i < size; i++)
-	    printf("%s %13s\n",PhoneBook[0].Name, PhoneBook[0].PhoneNumber);
+	for(int i = 0; i < size; i++)
+	    printf("%10s %13s\n",PhoneBook[i].Name, PhoneBook[i].PhoneNumber);
 }
 
